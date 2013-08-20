eea3da79fe3e308ee5e89ecb62a51495f828edf8
Ravindranath Akila <ravindranathakila@gmail.com>
2013-08-21 00:33:05 +0800

    Includes feed item by item rendering. Includes space support in feed url. Includes tts speach engine feed properly implemented.
    Yet to port changes upto the phonegap leve.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,20 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * // Feeds to retrieve
+ *  feeds: {
+ *     // identifier: url, ...
+ * },
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  // Maximum number of entries to fetch per feed, -1 for maximum available
+ *  max: -1,
+ *
+ *  // Use SSL connection. Option;
+ *  //  - true: use https
+ *  //  - false: use http
+ *  //  - 'auto': use same as current domain
+ *  ssl: 'auto',
+ *
+ *  // Retrieve and parse XML elements when true
+ *  xml: false,
+ *
+ *  // Called when all entries are rendered
+ *  onComplete: function( entries ) { },
+ *
+ *  // Called for each entry
+ *  preprocess: function( feed ) { },
+ *
+ *  // Template injected to container while feeds are loaded
+ *  loadingTemplate: '<p class="feeds-loader">Loading entries ...</p>',
+ *
+ *  // Template used to render each entry
+ *  entryTemplate:  '<div class="feeds-entry feeds-source-<!=source!>">' +
+ *  '<a class="feed-entry-title" target="_blank" href="<!=link!>" title="<!=title!>"><!=title!></a>' +
+ *  '<div class="feed-entry-date"><!=publishedDate!></div>' +
+ *  '<div class="feed-entry-content"><!=contentSnippet!></div>' +
+ *  '</div>'
+ *
+ *  title
+ *  author
+ *  publishedDate
+ *  content
+ *  contentSnippet (< 120 characters, no html tags)
+ *  link
+ *  mediaGroup
+ *  categories
+ *  source (the feed identifier, added by the plugin)
+ *  feedUrl (the url of the rss feed)
+ *  feedTitle (the title of the feed)
+ *  feedLink (the url of the HTML version of the feed)
+ *  feedDescription (the feed description)
+ *  feedAuthor (the feed author)
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 var app = {
     // Application Constructor
@@ -48,49 +88,72 @@ var app = {
     }
 };
 
-function unspeakFeed(){
+function unspeakFeed() {
     window.plugins.tts.stop();
 }
 
+var feedEntries;//Array of feed entries
+
 function speakFeed(rssFeedUrl) {
 
-    window.plugins.tts.speak("Reading your news!", function (arg) {
-        discoverFeedUrlFor(rssFeedUrl)
-            .done(function(data) {
-                var queryResult = data.responseData;
-                if (!!queryResult) {
-                    var feedUrl = queryResult.url;
-                    var feed = feedUrl;//'http://feeds.feedburner.com/techcrunch/social?format=xml';
+    try {
+        window.plugins.tts.speak("Reading your news!", function (arg) {
+            discoverFeedUrlFor(rssFeedUrl.replace(/\s+/g, ''))//We replace all spaces since a user can type something like Facebook.com which ends up with spaces in the end
+                .done(function (data) {
+                    feedEntries = [];
+                    var queryResult = data.responseData;
+                    if (!!queryResult) {
+                        var feedUrl = queryResult.url;
+                        var feed = feedUrl;//'http://feeds.feedburner.com/techcrunch/social?format=xml';
 
-                    $('#feed').feeds({
-                        feeds: {
-                            feed1: feed
-                        },
-                        preprocess: function (e) {
-                            try {
-                                window.plugins.tts.speak(this.title + "." + this.content,
-                                    function (arg) {
-                                    }, function (arg) {
-                                        alert(arg);
-                                    });
-
-                            } catch (error) {
-                                alert(error);
+                        $('#feed').feeds({
+                            feeds: {
+                                feed1: feed
+                            },
+                            preprocess: function (e) {
+                                try {
+                                    feedEntries.push(this.title + " - " + this.content);
+                                } catch (error) {
+                                    alert(error);
+                                }
+                            },
+                            onComplete: function () {
+                                try {
+                                    speakFeedEntriesRecursively();
+                                } catch (e) {
+                                    alert(e);
+                                }
                             }
-                        }
-                    });
-
-                }
-            });
-
-
-    }, function (arg) {
-        alert('Sorry, we are speechless! ' + arg.toString());
-    });
+                        });
+                    }
+                });
+        }, function (arg) {
+            alert('Sorry, we are speechless! ' + arg.toString());
+        });
+    } catch (e) {
+        alert(e);
+    }
 }
 
+function speakFeedEntriesRecursively() {
+    try {
+        if (feedEntries.length > 0) {
+            $('#feedNowSpeaking').empty();
+            $('#feedNowSpeaking').html(feedEntries[0]);
+            window.plugins.tts.speak(feedEntries[0],
+                function (arg) {
+                    feedEntries.shift(); //We are done, lets get rid of this entry
+                    speakFeedEntriesRecursively();
+                }, function (arg) {
+                    alert(arg);
+                });
+        }
+    } catch (e) {
+        alert(e);
+    }
+}
 
-var discoverFeedUrlFor = function(pageURL) {
+var discoverFeedUrlFor = function (pageURL) {
     var baseApiUrl = "http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0";
     var jQueryJsonpToken = "&callback=?"; // tells jQuery to treat it as JSONP request
     var pageUrlParameter = "&q=" + pageURL;
