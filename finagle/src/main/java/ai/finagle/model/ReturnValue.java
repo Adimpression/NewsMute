fffde29217b6abe1c7e91ed7b9ecf6117ed4c60b
Ravindranath Akila <ravindranathakila@gmail.com>
2013-10-27 00:19:32 +0800

    newsmute-51 Code device news renderer Data retrieval on proper json format and rendering complete.

---
+++ finagle/src/main/java/ai/finagle/model/ReturnValue.java
@@ -0,0 +1,15 @@
+package ai.finagle.model;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ * User: http://www.ilikeplaces.com
+ * Date: 26/10/13
+ * Time: 11:45 PM
+ */
+public class ReturnValue {
+    final YawnItem[] data;
+
+    public ReturnValue(final YawnItem[] data) {
+        this.data = data;
+    }
+}
