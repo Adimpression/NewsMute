66d0469cd49a2f14142bfc2931e381b6f7994405
Ravindranath Akila <ravindranathakila@gmail.com>
2013-10-27 13:49:40 +0800

    newsmute-41 Code client url submission and retrieval handling on server Every single person is, for now, a friend of the other. Done.

---
+++ finagle/src/main/java/ai/finagle/producer/Counsellor.java
@@ -0,0 +1,80 @@
+package ai.finagle.producer;
+
+import com.datastax.driver.core.*;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ * User: http://www.ilikeplaces.com
+ * Date: 27/10/13
+ * Time: 12:51 PM
+ */
+public class Counsellor implements Runnable {
+
+    private Cluster cluster;
+
+    @Override
+    public void run() {
+        this.open("10.208.27.21");
+
+        final Timer timer = new Timer();
+        final TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Counselling...");
+
+                    final Session connect = cluster.connect("Test1");
+
+                    final ResultSet executeScreamFetch = connect.execute("select * from Scream;");
+                    final ResultSet executeYawnFetch = connect.execute("select * from Yawn;");
+                    final List<Row> allScreams = executeScreamFetch.all();
+                    final List<Row> allYawns = executeYawnFetch.all();
+
+                    System.out.println("Counselling " + allScreams.size() + " screams");
+                    System.out.println("Counselling " + allYawns.size() + " yawns");
+
+                    int totalInsertions = 0;
+
+                    for (final Row scream : allScreams) {
+                        for (final Row friends : allScreams) {//Ideally, all screams are not friends of this person, but we do so for now for testing
+                            connect.execute("insert into Yawn(humanId, urlHash, value) values('" + scream.getString("humanId") + "','" + friends.getString("urlHash") + "','" + friends.getString("value") + "');");
+                            totalInsertions++;
+                        }
+                    }
+
+                    System.out.println("Counselled successfully " + totalInsertions + " sessions");
+                } catch (final Exception e) {
+                    e.printStackTrace(System.err);
+                }
+
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 10000);
+    }
+
+    public String open(String node) {
+        cluster = Cluster.builder()
+                .addContactPoint(node)
+                .build();
+        cluster.connect();
+        Metadata metadata = cluster.getMetadata();
+        System.out.printf("Connected to cluster: %s\n",
+                metadata.getClusterName());
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (Host host : metadata.getAllHosts()) {
+            System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+                    host.getDatacenter(), host.getAddress(), host.getRack());
+            stringBuilder.append("Datacenter: ").append(host.getDatacenter()).append("; Host: ").append(host.getAddress()).append("; Rack: ").append(host.getRack());
+        }
+        return stringBuilder.toString();
+    }
+
+    public void close() {
+        cluster.shutdown();
+    }
+}
---
+++ finagle/src/main/java/run.java
@@ -0,0 +1,37 @@
+import ai.finagle.producer.Counsellor;
+import ai.finagle.producer.Screamer;
+import ai.finagle.producer.Yawner;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ * User: http://www.ilikeplaces.com
+ * Date: 27/10/13
+ * Time: 12:16 PM
+ */
+public class run {
+    public static void main(final String args[]) {
+        StartThreadSafely(new Thread(new Screamer()));
+        StartThreadSafely(new Thread(new Yawner()));
+        StartThreadSafely(new Thread(new Counsellor()));
+    }
+
+    private static void StartThreadSafely(final Thread thread) {
+        try {
+            thread.run();
+        } catch (final Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+}
+
+//        final ResultSet execute = connect.execute("CREATE KEYSPACE Test WITH strategy_class = 'SimpleStrategy' AND strategy_options:replication_factor = 1;");
+//        try {
+//            final ResultSet execute = connect.execute("CREATE KEYSPACE Test1 WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};");
+//            for (Row row : execute.all()) {
+//                System.out.println("Create Keyspace Result:" + row.toString());
+//            }
+//        } catch (AlreadyExistsException e) {
+//            System.out.println("Keyspace exists. Hence using it.");
+//            final Session test = cluster.connect("Test1");
+//        }
