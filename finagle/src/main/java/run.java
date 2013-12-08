4e22f2e293f8df9e1902bc9bbdd43ab81000085d
Ravindranath Akila <ravindranathakila@gmail.com>
2013-12-08 20:31:57 +0800

    First, to make things clean, now we get an additional command line parameter to resolve what to do. Having everything in one jar makes life easy until we stabilize. #newsmute-85 In Progress

--- finagle/src/main/java/run.java
+++ finagle/src/main/java/run.java
@@ -1,12 +1,39 @@
 import ai.finagle.producer.*;
 
+enum WHAT_TO_RUN {
+    SCREAMER,
+    YAWNER,
+    COUNSELLOR,
+    STALKER,
+    HARVESTER,
+    SUPER_FRIENDER,
+}
+
 /**
+ * We are a bit confused what to do next. So here's the deal:
+ * <p/>
+ * This class will have 3 services exposed for user based triggers.
+ * Ideally running in tens of instances to handle the load.
+ * That is, http requests:
+ * <ul>
+ * <li>Screamer</li>
+ * <li>Yawner</li>
+ * <li>Stalker</li>
+ * <li>SuperFriender</li>
+ * </ul>
+ * <p/>
+ * WHere as there are 2 other services, ideally running as one instance
+ * <ul>
+ * <li>Counsellor</li>
+ * <li>Harvester</li>
+ * </ul>
+ * <p/>
  * You want to know how we do it? We just do
  * "java -cp . Finagle.jar run"
  * "java -cp . Finagle.jar test" //See test class
- *
+ * <p/>
  * Thought it was a cool way to do things and keep it memorable.
- *
+ * <p/>
  * Created with IntelliJ IDEA Ultimate.
  * User: http://NewsMute.com
  * Date: 27/10/13
@@ -14,14 +41,44 @@ import ai.finagle.producer.*;
  */
 public class run {
     public static void main(final String args[]) {
-        StartThreadSafely(new Thread(new Screamer()));
-        StartThreadSafely(new Thread(new Yawner()));
-        StartThreadSafely(new Thread(new Counsellor()));
-
-        StartThreadSafely(new Thread(new SuperFriender()));
-
-        StartThreadSafely(new Thread(new Stalker()));
-        StartThreadSafely(new Thread(new Harvester()));
+        try {
+            for (final String arg : args) {
+                final WHAT_TO_RUN what_to_run = WHAT_TO_RUN.valueOf(arg.toUpperCase());
+                System.out.println("Executing " + what_to_run.toString());
+                switch (what_to_run) {
+                    case SCREAMER:
+                        StartThreadSafely(new Thread(new Screamer()));
+                        break;
+                    case YAWNER:
+                        StartThreadSafely(new Thread(new Yawner()));
+                        break;
+                    case COUNSELLOR:
+                        StartThreadSafely(new Thread(new Counsellor()));
+                        break;
+                    case STALKER:
+                        StartThreadSafely(new Thread(new Stalker()));
+                        break;
+                    case HARVESTER:
+                        StartThreadSafely(new Thread(new Harvester()));
+                        break;
+                    case SUPER_FRIENDER:
+                        StartThreadSafely(new Thread(new SuperFriender()));
+                        break;
+                    default:
+                        final StringBuilder options = new StringBuilder("");
+                        for (final WHAT_TO_RUN runOptions : WHAT_TO_RUN.values()) {
+                            options.append("\n").append(runOptions.toString());
+                        }
+                        throw new UnsupportedOperationException("To run, provide one or many of the following, separated by spaces. Case doesn't matter. " + options.toString());
+                }
+            }
+        } catch (final Throwable t) {
+            final StringBuilder options = new StringBuilder("");
+            for (final WHAT_TO_RUN what_to_run : WHAT_TO_RUN.values()) {
+                options.append("\n").append(what_to_run.toString());
+            }
+            throw new RuntimeException("To run, provide one or many of the following, separated by spaces. Case doesn't matter. " + options.toString(), t);
+        }
     }
 
     private static void StartThreadSafely(final Thread thread) {
