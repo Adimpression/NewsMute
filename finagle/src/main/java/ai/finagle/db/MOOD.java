1e6929bf9fb5953c39519b5ed036c5125e6a0013
Ravindranath Akila <ravindranathakila@gmail.com>
2013-12-21 15:38:15 +0800

    In a bad mood. Redefining mood #-|  mood }-D #newsmute-76 In Progress

---
+++ finagle/src/main/java/ai/finagle/db/MOOD.java
@@ -0,0 +1,35 @@
+package ai.finagle.db;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ * User: http://NewsMute.com
+ * Date: 21/12/13
+ * Time: 3:30 PM
+ */
+public class MOOD {
+
+    /**
+     * This signifies the state of an entry on the row, deleted or live.
+     * There is a TTL correspondence since, a deleted entry can spam the system.
+     * So we keep it with state {@link ai.finagle.db.MOOD.LIFE#DEAD} for some time.
+     * <ul>
+     *     <li>Deals with TTL of cassandra.</li>
+     *     <li>Deals with removal and adding of entries</li>
+     * </ul>
+     */
+    public enum LIFE{
+        DEAD('1'),
+        ALIVE('0');
+
+        public final char value;
+
+        LIFE(final char value) {
+            this.value = value;
+        }
+
+    }
+
+    public enum FAME{
+
+    }
+}
