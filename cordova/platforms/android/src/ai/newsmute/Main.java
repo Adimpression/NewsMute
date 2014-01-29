822d03e76c6c8d2107cea7c298c738fac95edc99
Ravindranath Akila <ravindranathakila@gmail.com>
2014-01-30 01:13:15 +0800

    Back button now takes to home #newsmute-57 In Progress

--- cordova/platforms/android/src/ai/newsmute/Main.java
+++ cordova/platforms/android/src/ai/newsmute/Main.java
@@ -19,8 +19,12 @@
 
 package ai.newsmute;
 
-import android.graphics.Color;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import org.apache.cordova.Config;
 import org.apache.cordova.DroidGap;
@@ -29,18 +33,47 @@ public class Main extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
         super.init();
+
+        final WebView mWebView = super.appView;
+
+        final Activity activity = this;
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+
+        });
+
         super.appView.clearCache(true);
 
         //super.appView.setBackgroundColor(Color.TRANSPARENT);
         //super.appView.setBackgroundColor(0x00000000);
         //super.appView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
 
+
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
 
-    }
+        super.appView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(final View v, final int keyCode, final KeyEvent event) {
+// Check if the key event was the Back button and if there's history
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    //appView.goBack();
+                    Main.super.loadUrl(Config.getStartUrl());
+                    return true;
+                } else {
+                    // If it wasn't the Back key or there's no web page history, bubble up to the default
+                    // system behavior (probably exit the activity)
+                    return Main.super.onKeyDown(keyCode, event);
+                }
+            }
+        });
 
+    }
 }
 
