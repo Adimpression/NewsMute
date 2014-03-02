2f1966da102a74f585eaafe00eee9426ff9b49d4
Ravindranath Akila <ravindranathakila@gmail.com>
2014-03-02 16:26:43 +0800

    http://NewsMute.com now has a more increased time to live count on inserts. We have also added scream duration to be two weeks. The latter change is a feature on the users side. The former ist o overcome tombstone limitations. #newsmute-133 In Progress

--- finagle/src/main/java/ai/finagle/db/DBScripts.java
+++ finagle/src/main/java/ai/finagle/db/DBScripts.java
@@ -19,14 +19,14 @@ public class DBScripts {
 
     public static final String CREATE_SESSION = "create table Session(sessionId varchar, value varchar,  PRIMARY KEY (sessionId));";
 
-    public static final int YAWN_TTL = 600;
-    public static final int YAWN_COUNSELLED_TTL = 600;
+    public static final int YAWN_TTL = 86400 * 14;//Fourteen days //(Please take into account Cassandra tombstone limitations on queue like implementations)
+    public static final int YAWN_COUNSELLED_TTL = 600; //(Please take into account Cassandra tombstone limitations on queue like implementations)
     public static final int YAWN_COUNSELLOR_REINCARNATION = 60000;//millis
-    public static final int YAWN_READED_TTL = 86400 * 14;//Fourteen days
-    public static final int SESSION_TTL = 86400 * 14;//Fourteen days//Don't go higher, in case a software update makes sessions inconsistent we can atleast recover in two weeks
+    public static final int YAWN_READED_TTL = 86400 * 14;//Fourteen days //(Please take into account Cassandra tombstone limitations on queue like implementations)
+    public static final int SESSION_TTL = 86400 * 14;//Fourteen days//Don't go higher, in case a software update makes sessions inconsistent we can atleast recover in two weeks //(Please take into account Cassandra tombstone limitations on queue like implementations)
 
     public static final int STALK_HARVESTER_REINCARNATION = 600000;
 
-    public static final Object HARVESTED_YAWN_TTL = 86400 * 1;//One day
-    public static final Object INITIAL_INSERT_TTL = 1200;
+    public static final Object HARVESTED_YAWN_TTL = 86400 * 1;//One day //(Please take into account Cassandra tombstone limitations on queue like implementations)
+    public static final Object INITIAL_INSERT_TTL = 86400 * 1;//One day //(Please take into account Cassandra tombstone limitations on queue like implementations)
 }
