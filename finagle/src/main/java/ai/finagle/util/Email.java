966785987880ffb6bb9d4bb37994ff3bcda24083
Ravindranath Akila <ravindranathakila@gmail.com>
2014-05-07 16:46:59 +0800

    http://NewsMute.com improvements with refactoring #newsmute-133 In Progress

--- finagle/src/main/java/ai/finagle/util/Email.java
+++ finagle/src/main/java/ai/finagle/util/Email.java
@@ -1,8 +1,6 @@
 package ai.finagle.util;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.*;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
@@ -17,18 +15,27 @@ import javax.ws.rs.core.MediaType;
 public class Email {
 
     public static ClientResponse sendText(final String from, final String to, final String subject, final String body) {
-        Client client = Client.create();
-        client.addFilter(new HTTPBasicAuthFilter("api",
-                "key-3ax6xnjp29jd6fds4gc373sgvjxteol0"));
-        WebResource webResource =
-                client.resource("https://api.mailgun.net/v2/samples.mailgun.org" +
-                        "/messages");
-        MultivaluedMapImpl formData = new MultivaluedMapImpl();
-        formData.add("from", from);//"Verify Email <emailcheck@mutenews.com>"
-        formData.add("to", to);
-        formData.add("subject", subject);
-        formData.add("text", body);
-        return webResource.type(MediaType.APPLICATION_FORM_URLENCODED).
-                post(ClientResponse.class, formData);
+        try {
+            Client client = Client.create();
+            client.addFilter(new HTTPBasicAuthFilter("api",
+                    "key-"));
+            WebResource webResource =
+                    client.resource("https://api.mailgun.net/v2/mutenews.com" +
+                            "/messages");
+            MultivaluedMapImpl formData = new MultivaluedMapImpl();
+            formData.add("from", from);//"Verify Email <emailcheck@mutenews.com>"
+            formData.add("to", to);
+            formData.add("subject", subject);
+            formData.add("text", body);
+            final ClientResponse response = webResource.type(MediaType.APPLICATION_FORM_URLENCODED).
+                    post(ClientResponse.class, formData);
+
+            response.bufferEntity();
+            System.out.println(response.getEntity(String.class));
+            return response;
+        } catch (final Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
