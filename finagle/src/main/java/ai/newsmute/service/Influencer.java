a489c01ac6f87cec2e927dff9a7b4f78e9658780
Ravindranath Akila <ravindranathakila@gmail.com>
2015-02-07 12:26:48 +0800

    http://NewsMute.com Added all countries #newsmute-133 In Progress

--- finagle/src/main/java/ai/newsmute/service/Influencer.java
+++ finagle/src/main/java/ai/newsmute/service/Influencer.java
@@ -4,16 +4,15 @@ import ai.newsmute.db.DBScripts;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 /**
  * Phase 1:
@@ -44,6 +43,203 @@ public class Influencer implements Runnable {
 
     final static String endpointGodFather = "http://guardian.newsmute.com:40700";
 
+    final String[] countries = new String[]{"Afghanistan",
+            "Albania",
+            "Algeria",
+            "Andorra",
+            "Angola",
+            "Argentina",
+            "Armenia",
+            "Australia",
+            "Austria",
+            "Azerbaijan",
+            "Bahamas",
+            "Bahrain",
+            "Bangladesh",
+            "Barbados",
+            "Belarus",
+            "Belgium",
+            "Belize",
+            "Benin",
+            "Bhutan",
+            "Bolivia",
+            "BosniaHerzegovina",
+            "Botswana",
+            "Brazil",
+            "Brunei",
+            "Bulgaria",
+            "Burkina",
+            "Burundi",
+            "Cambodia",
+            "Cameroon",
+            "Canada",
+            "CapeVerde",
+            "CentralAfricanRep",
+            "Chad",
+            "Chile",
+            "China",
+            "Colombia",
+            "Comoros",
+            "Congo",
+            "CongoDemocraticRep",
+            "CostaRica",
+            "Croatia",
+            "Cuba",
+            "Cyprus",
+            "CzechRepublic",
+            "Denmark",
+            "Djibouti",
+            "Dominica",
+            "DominicanRepublic",
+            "EastTimor",
+            "Ecuador",
+            "Egypt",
+            "ElSalvador",
+            "EquatorialGuinea",
+            "Eritrea",
+            "Estonia",
+            "Ethiopia",
+            "Fiji",
+            "Finland",
+            "France",
+            "Gabon",
+            "Gambia",
+            "Georgia",
+            "Germany",
+            "Ghana",
+            "Greece",
+            "Grenada",
+            "Guatemala",
+            "Guinea",
+            "Guinea-Bissau",
+            "Guyana",
+            "Haiti",
+            "Honduras",
+            "Hungary",
+            "Iceland",
+            "India",
+            "Indonesia",
+            "Iran",
+            "Iraq",
+            "IrelandRepublic",
+            "Israel",
+            "Italy",
+            "IvoryCoast",
+            "Jamaica",
+            "Japan",
+            "Jordan",
+            "Kazakhstan",
+            "Kenya",
+            "Kiribati",
+            "KoreaNorth",
+            "KoreaSouth",
+            "Kosovo",
+            "Kuwait",
+            "Kyrgyzstan",
+            "Laos",
+            "Latvia",
+            "Lebanon",
+            "Lesotho",
+            "Liberia",
+            "Libya",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Macedonia",
+            "Madagascar",
+            "Malawi",
+            "Malaysia",
+            "Maldives",
+            "Mali",
+            "Malta",
+            "MarshallIslands",
+            "Mauritania",
+            "Mauritius",
+            "Mexico",
+            "Micronesia",
+            "Moldova",
+            "Monaco",
+            "Mongolia",
+            "Montenegro",
+            "Morocco",
+            "Mozambique",
+            "Myanmar,{Burma}",
+            "Namibia",
+            "Nauru",
+            "Nepal",
+            "Netherlands",
+            "NewZealand",
+            "Nicaragua",
+            "Niger",
+            "Nigeria",
+            "Norway",
+            "Oman",
+            "Pakistan",
+            "Palau",
+            "Panama",
+            "PapuaNewGuinea",
+            "Paraguay",
+            "Peru",
+            "Philippines",
+            "Poland",
+            "Portugal",
+            "Qatar",
+            "Romania",
+            "RussianFederation",
+            "Rwanda",
+            "StKitts&Nevis",
+            "StLucia",
+            "SaintVincent&theGrenadines’",
+            "Samoa",
+            "SanMarino",
+            "SaoTome&Principe",
+            "SaudiArabia",
+            "Senegal",
+            "Serbia",
+            "Seychelles",
+            "SierraLeone",
+            "Singapore",
+            "Slovakia",
+            "Slovenia",
+            "SolomonIslands",
+            "Somalia",
+            "SouthAfrica",
+            "Spain",
+            "SriLanka",
+            "Sudan",
+            "Suriname",
+            "Swaziland",
+            "Sweden",
+            "Switzerland",
+            "Syria",
+            "Taiwan",
+            "Tajikistan",
+            "Tanzania",
+            "Thailand",
+            "Togo",
+            "Tonga",
+            "Trinidad&Tobago",
+            "Tunisia",
+            "Turkey",
+            "Turkmenistan",
+            "Tuvalu",
+            "Uganda",
+            "Ukraine",
+            "UnitedArabEmirates",
+            "UnitedKingdom",
+            "UnitedStates",
+            "Uruguay",
+            "Uzbekistan",
+            "Vanuatu",
+            "VaticanCity",
+            "Venezuela",
+            "Vietnam",
+            "Yemen",
+            "Zambia",
+            "Zimbabwe",
+    };
+
+
     public Influencer() {
     }
 
@@ -57,63 +253,73 @@ public class Influencer implements Runnable {
                     final Date startTime = Calendar.getInstance().getTime();
                     System.out.println(String.format("Influencing started at %s...", new SimpleDateFormat("MM-dd HH:mm:ss").format(startTime)));
 
-                    //Get complete hashed password in hex format
+                    final List<String> failed = new ArrayList<String>();
 
-                    final String email = "@adimpression.mobi";
-                    final String passwordHash = get_hash("");
+                    for (final String country : countries) {
+                        try {
+                            System.out.println("Influencing: " + country);
 
-                    final String tokenHash;
-                    {
-                        final String login = endpointGuardian + "/?user=" + get_hash(email) + "&token=" + passwordHash + "&nmact=" + "READ";
-                        final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login).asJson();
-                        final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
-                        final JSONObject status = body.getJSONObject("returnValue");
-                        tokenHash = status.getJSONArray("data").getJSONObject(0).getString("tokenHash");
-                    }
+                            final String email = country.toLowerCase() + "@adimpression.mobi";
+                            final String passwordHash = get_hash("");
 
-                    final JSONArray data;
-                    {
-                        final String login = endpointYawn + "/?user=" + get_hash(email) + "&nmact=" + "READ";
-                        final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
-                                .header("x-session-header", tokenHash)
-                                .asJson();
-                        final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
-                        System.out.println(body.toString());
-                        final JSONObject status = body.getJSONObject("returnValue");
-                        data = status.getJSONArray("data");
-                    }
-
-                    {
-                        for (int i = 0; i < data.length(); i++) {
-                            final String link = data.getJSONObject(i).getString("link");
+                            final String tokenHash;
                             {
-                                {
-                                    final String login = endpointScream;
-                                    final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
-                                            .queryString("user", get_hash(email))
-                                            .queryString("url", link)
-                                            .header("x-session-header", tokenHash)
-                                            .asJson();
-                                    final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
-                                    System.out.println(body.toString());
-                                }
-                                {
-                                    final String login = endpointYawn;
-                                    final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
-                                            .queryString("user", get_hash(email))
-                                            .queryString("url", link)
-                                            .queryString("nmact", "DELETE")
-                                            .header("x-session-header", tokenHash)
-                                            .asJson();
-                                    final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
-                                    System.out.println(body.toString());
+                                final String login = endpointGuardian + "/?user=" + get_hash(email) + "&token=" + passwordHash + "&nmact=" + "READ";
+                                final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login).asJson();
+                                final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
+                                final JSONObject status = body.getJSONObject("returnValue");
+                                tokenHash = status.getJSONArray("data").getJSONObject(0).getString("tokenHash");
+                            }
+
+                            final JSONArray data;
+                            {
+                                final String login = endpointYawn + "/?user=" + get_hash(email) + "&nmact=" + "READ";
+                                final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
+                                        .header("x-session-header", tokenHash)
+                                        .asJson();
+                                final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
+                                System.out.println(body.toString());
+                                final JSONObject status = body.getJSONObject("returnValue");
+                                data = status.getJSONArray("data");
+                            }
+
+                            {
+                                for (int i = 0; i < data.length(); i++) {
+                                    final String link = data.getJSONObject(i).getString("link");
+                                    {
+                                        {
+                                            final String login = endpointScream;
+                                            final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
+                                                    .queryString("user", get_hash(email))
+                                                    .queryString("url", link)
+                                                    .header("x-session-header", tokenHash)
+                                                    .asJson();
+                                            final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
+                                            System.out.println(body.toString());
+                                        }
+                                        {
+                                            final String login = endpointYawn;
+                                            final HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post(login)
+                                                    .queryString("user", get_hash(email))
+                                                    .queryString("url", link)
+                                                    .queryString("nmact", "DELETE")
+                                                    .header("x-session-header", tokenHash)
+                                                    .asJson();
+                                            final JSONObject body = jsonNodeHttpResponse.getBody().getObject();
+                                            System.out.println(body.toString());
+                                        }
+                                    }
                                 }
                             }
+                        } catch (final Throwable e) {
+                            failed.add(country);
+                            //e.printStackTrace(System.out);
                         }
                     }
 
-
                     final Date endTime = Calendar.getInstance().getTime();
+
+                    System.out.println("Failed:" + Arrays.toString(failed.toArray()));
                     System.out.printf("Influencing finished at %s influencing %d sessions", new SimpleDateFormat("MM-dd HH:mm:ss").format(endTime), 0);
                     System.out.println("Influencing took " + (endTime.getTime() - startTime.getTime()) + "  milliseconds");
 
@@ -123,7 +329,7 @@ public class Influencer implements Runnable {
             }
         };
 
-        timer.scheduleAtFixedRate(task, 0, DBScripts.STALK_HARVESTER_REINCARNATION);//Every ten minutes
+        timer.scheduleAtFixedRate(task, 0, 60000 * 60);//Every ten minutes
     }
 
     private String get_hash(final String email) throws NoSuchAlgorithmException {
