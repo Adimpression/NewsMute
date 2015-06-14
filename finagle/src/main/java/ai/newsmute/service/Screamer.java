9eb6cf3a38336ea465491acc897b6b3e59e3740d
Ravindranath Akila <ravindranathakila@gmail.com>
2015-06-14 18:21:45 +0800

    http://NewsMute.com Adding DynamoDB support: Convertned Screamer #newsmute-304 In Progress

--- finagle/src/main/java/ai/newsmute/service/Screamer.java
+++ finagle/src/main/java/ai/newsmute/service/Screamer.java
@@ -7,6 +7,7 @@ import ai.newsmute.model.Return;
 import ai.newsmute.model.ReturnValueScream;
 import ai.newsmute.model.YawnItem;
 import ai.newsmute.util.Printer;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.datastax.driver.core.*;
 import com.google.gson.Gson;
 import com.twitter.finagle.Service;
@@ -24,6 +25,7 @@ import org.jsoup.nodes.Element;
 import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.InetSocketAddress;
 import java.net.URL;
@@ -50,9 +52,17 @@ public class Screamer implements Runnable {
 
     private final String databaseIp;
 
+    @Autowired
+    public DBScripts.DB db;
+
     private Cluster cluster;
 
     private Session threadSafeSession;
+
+    private DynamoDB dynamoDB;
+
+    private Table tableScream;
+    private Table tableYawn;
 
     public Screamer(final String bindIp, final String port, final String databaseIp) {
         this.bindIp = bindIp;
@@ -66,13 +76,21 @@ public class Screamer implements Runnable {
      */
     @Override
     public void run() {
-        this.open(databaseIp);
+        switch (db) {
 
-        final Session connect = cluster.connect("NewsMute");
-        try {
-            connect.execute(DBScripts.CREATE_SCREAM);
-        } catch (final Exception e) {//Table already exists
-            LOG.info(e.getMessage());
+            case DynamoDB:
+                break;
+            case Cassandra:
+                    this.open(databaseIp);
+
+                    final Session connect = cluster.connect("NewsMute");
+                    try {
+                        connect.execute(DBScripts.CREATE_SCREAM);
+                    } catch (final Exception e) {//Table already exists
+                        LOG.info(e.getMessage());
+                    }
+                break;
+            default: throw new UnsupportedOperationException("Unknown DB Type:" + db);
         }
 
 
@@ -124,8 +142,19 @@ public class Screamer implements Runnable {
                         final String hashedUser = BCrypt.hashpw(unhashedUser, SuperFriender.GLOBAL_SALT);
                         LOG.info("hashed user:" + hashedUser);
 
-                        final List<Row> screamRowsCounselled = threadSafeSession.execute(String.format("select * from Yawn where humanId='%s' AND mood='%c' AND urlHash='%s'", hashedUser, MOOD.LIFE.DEAD.state, s)).all();
-                        if(screamRowsCounselled.isEmpty()){
+                        final boolean noRecord;
+                        switch (db) {
+                            case DynamoDB:
+                                noRecord = tableYawn.getItem(new PrimaryKey().addComponents(new KeyAttribute("humanId", hashedUser), new KeyAttribute("mood", MOOD.LIFE.DEAD.state), new KeyAttribute("urlHash", s))) == null;
+                                break;
+                            case Cassandra:
+                                final List<Row> screamRowsCounselled = threadSafeSession.execute(String.format("select * from Yawn where humanId='%s' AND mood='%c' AND urlHash='%s'", hashedUser, MOOD.LIFE.DEAD.state, s)).all();
+                                noRecord = screamRowsCounselled.isEmpty();
+                                break;
+                            default: throw new UnsupportedOperationException("Unknown DB Type:" + db);
+                        }
+
+                        if(noRecord){
                             try {
                                 final Document document = Jsoup.parse(new URL(s).openStream(), "UTF-8", s);
 
@@ -139,10 +168,33 @@ public class Screamer implements Runnable {
                                     }
                                 }
                                 LOG.info("description:" + description);
-                                threadSafeSession.execute(String.format("insert into Scream(humanId, mood, urlHash, value) values('%s','%c','%s','%s') USING TTL %d;", hashedUser, MOOD.LIFE.ALIVE.state, s, new Gson().toJson(new YawnItem(s, title, description, hashedUser, "0")), DBScripts.YAWN_COUNSEL));//Yet to hash the urlHash value
+
+                                final String value = new Gson().toJson(new YawnItem(s, title, description, hashedUser, "0"));
+
+                                switch (db) {
+                                    case DynamoDB:
+                                        tableScream.putItem(new Item().withPrimaryKey(new PrimaryKey(new KeyAttribute("humanId", hashedUser),new KeyAttribute("mood", MOOD.LIFE.ALIVE.state), new KeyAttribute("urlHash", s))).withString("value", value));
+                                        break;
+                                    case Cassandra:
+                                        threadSafeSession.execute(String.format("insert into Scream(humanId, mood, urlHash, value) values('%s','%c','%s','%s') USING TTL %d;", hashedUser, MOOD.LIFE.ALIVE.state, s, value, DBScripts.YAWN_COUNSEL));//Yet to hash the urlHash value
+                                        break;
+                                    default: throw new UnsupportedOperationException("Unknown DB Type:" + db);
+                                }
+
                             } catch (final Throwable e) {//Leave the insert here alone. Works as a default if the internet connectivity decides to break. We discovered this when application was running on a non www accessible server
                                 e.printStackTrace(System.out);
-                                threadSafeSession.execute(String.format("insert into Scream(humanId, mood, urlHash, value) values('%s','%c','%s','%s') USING TTL %d;", hashedUser, MOOD.LIFE.ALIVE.state, s, new Gson().toJson(new YawnItem(s, s, s, hashedUser, "0")), DBScripts.YAWN_COUNSEL));//Yet to hash the urlHash value
+
+                                final String defaultValue = new Gson().toJson(new YawnItem(s, s, s, hashedUser, "0"));
+
+                                switch (db) {
+                                    case DynamoDB:
+                                        tableScream.putItem(new Item().withPrimaryKey(new PrimaryKey(new KeyAttribute("humanId", hashedUser),new KeyAttribute("mood", MOOD.LIFE.ALIVE.state), new KeyAttribute("urlHash", s))).withString("value", defaultValue));
+                                        break;
+                                    case Cassandra:
+                                        threadSafeSession.execute(String.format("insert into Scream(humanId, mood, urlHash, value) values('%s','%c','%s','%s') USING TTL %d;", hashedUser, MOOD.LIFE.ALIVE.state, s, defaultValue, DBScripts.YAWN_COUNSEL));//Yet to hash the urlHash value
+                                        break;
+                                    default: throw new UnsupportedOperationException("Unknown DB Type:" + db);
+                                }
                             }
                         } else{
                             LOG.info("Ignoring already screamed item for humanId:" + hashedUser + " for url:" + s);
