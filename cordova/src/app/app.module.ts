3b0aa1c07983a9eae27bae96235654bc461a2cfc
ravindranathakila <ravindranathakila@gmail.com>
2017-11-19 01:17:46 +0800

    Navigation etc.

--- cordova/src/app/app.module.ts
+++ cordova/src/app/app.module.ts
@@ -1,30 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {SigninPage} from "../pages/signin/signin";
+import {SettingsPage} from "../pages/settings/settings";
+import {SignupPage} from "../pages/signup/signup";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        SigninPage,
+        SignupPage,
+        HomePage,
+        SettingsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        SigninPage,
+        SignupPage,
+        HomePage,
+        SettingsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- cordova/src/pages/signin/signin.ts
+++ cordova/src/pages/signin/signin.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {SignupPage} from "../signup/signup";
 
 /**
  * Generated class for the SigninPage page.
@@ -15,11 +16,58 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class SigninPage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    public signupPage = SignupPage;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad SigninPage');
     }
 
+    go(page) {
+        this.navCtrl.push(page).catch(
+            e => {
+                console.error(e.message);
+            })
+    }
+
+
+    awsCognitoLoginNewsMute() {
+        let alert = this.alertCtrl.create({
+            title: 'Login',
+            inputs: [
+                {
+                    name: 'username',
+                    placeholder: 'Username'
+                },
+                {
+                    name: 'password',
+                    placeholder: 'Password',
+                    type: 'password'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Login',
+                    handler: data => {
+                        // if (User.isValid(data.username, data.password)) {
+                        //     // logged in!
+                        // } else {
+                        //     // invalid login
+                        //     return false;
+                        // }
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 }
